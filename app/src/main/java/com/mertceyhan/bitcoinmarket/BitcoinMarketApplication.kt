5c99d278e2347afcb807f521059876dd287007bc
Rahul16875 <rahulagrawal16875@gmail.com>
2024-07-12 02:38:19 +0530

    commit

--- app/src/main/java/com/mertceyhan/bitcoinmarket/BitcoinMarketApplication.kt
+++ app/src/main/java/com/mertceyhan/bitcoinmarket/BitcoinMarketApplication.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BitcoinApplication : Application()
+class BitcoinMarketApplication : Application()
 
